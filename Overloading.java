bd62464ff2e32ecbb53333396483180879ff0045
Udeen <najmuddinfaiz26@gmail.com>
2026-03-15 22:56:39 +0700

    Add Overloading class with area calculation methods

---
+++ Overloading.java
@@ -0,0 +1,38 @@
+//Muhammad Faiz Najmuddin
+//255150707111013
+
+import java.util.Scanner;
+
+public class Overloading {
+
+    public static void HitungLuas(int a, int b) {
+        System.out.println("maka hasil luas : " + (a * b));
+    }
+
+    public static double HitungLuas(double value, double value2) {
+        return value * value2;
+    }
+
+    public static void HitungLuasVoid(double value, double value2) {
+        System.out.println("maka hasil luas : " + (value * value2));
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("masukkan nilai integer 1 : ");
+        int integer1 = in.nextInt();
+        System.out.print("masukkan nilai integer 2 : ");
+        int integer2 = in.nextInt();
+        HitungLuas(integer1, integer2);
+        System.out.print("masukkan nilai double 1 : ");
+        double double1 = in.nextDouble();
+        System.out.print("masukkan nilai double 2 : ");
+        double double2 = in.nextDouble();
+        System.out.println("Maka hasil luas : " + HitungLuas(double1, double2));
+
+        // pertanyaan A3 - panggil yang versi void
+        HitungLuasVoid(double1, double2);
+
+        in.close();
+    }
+}
