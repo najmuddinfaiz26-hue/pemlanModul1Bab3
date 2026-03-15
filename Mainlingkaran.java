0fa68dd82c7ecf914f6197f41e9ad6b70c89d30c
Udeen <najmuddinfaiz26@gmail.com>
2026-03-15 22:57:51 +0700

    Add Mainlingkaran class with main method
    Implement Mainlingkaran class to demonstrate lingkaran usage.

---
+++ Mainlingkaran.java
@@ -0,0 +1,21 @@
+//Muhammad Faiz Najmuddin
+//255150707111013
+
+public class Mainlingkaran {
+    public static void main(String[] args) {
+
+        
+        lingkaran l = new lingkaran(3);
+        l.setTinggi(10);
+        l.displayMessage();
+
+        lingkaran l2 = new lingkaran(4, 10);
+        l2.displayMessage();
+
+        
+
+       
+        lingkaran Lstring = new lingkaran("5", "12");
+        Lstring.displayMessage();
+    }
+}
